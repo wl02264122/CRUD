713e686f1c7a43f597b0e7af9dc88fc2f23d45d1
wl02264122 <a1536561939@gmail.com>
2024-08-22 18:47:01 +0800

    Apped login page

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Create
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
-import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js";
+import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCwGdR-3aFaKU6OY_qZDbbyyX9E5vkFOOg",
@@ -12,43 +12,63 @@ const firebaseConfig = {
     databaseURL: "https://github-crud-ed61b-default-rtdb.asia-southeast1.firebasedatabase.app"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
+const usersRef = ref(database, 'users');
+
 let submit = document.querySelector(".submit-box");
+let login = document.querySelector(".login-box");
 
 const data = {
     create() {
-        submit.addEventListener("click", (e) => {
-            let emailInfo = document.querySelector("#email").value;
-            let usernameInfo = document.querySelector("#username").value;
-            let passwordInfo = document.querySelector("#password").value;
-            if (!emailInfo && !usernameInfo && !passwordInfo) {
-                alert("All fields are required!");
-                return;
-            }
-            else if (!emailInfo) {
-                alert("email cannot be empty.");
-                return;
-            } else if (!usernameInfo) {
-                alert("Username cannot be empty.")
-                return;
-            } else if (!passwordInfo) {
-                alert("Password cannot be empty.")
-                return;
-            }
-            set(ref(database, 'users/' + usernameInfo), {
-                email: emailInfo,
-                username: usernameInfo,
-                password: passwordInfo
-            })
-                .then(function () {
-                    alert("Register success");
-                }).catch(function (error) {
-                    alert("Error:" + error);
+        try {
+            submit.addEventListener("click", (e) => {
+                let emailInfo = document.querySelector("#email").value;
+                let usernameInfo = document.querySelector("#username").value;
+                let passwordInfo = document.querySelector("#password").value;
+                if (!emailInfo && !usernameInfo && !passwordInfo) {
+                    alert("All fields are required!");
+                    return;
+                }
+                else if (!emailInfo) {
+                    alert("email cannot be empty.");
+                    return;
+                } else if (!usernameInfo) {
+                    alert("Username cannot be empty.")
+                    return;
+                } else if (!passwordInfo) {
+                    alert("Password cannot be empty.")
+                    return;
+                }
+                set(ref(database, 'users/' + usernameInfo), {
+                    email: emailInfo,
+                    username: usernameInfo,
+                    password: passwordInfo
                 })
+                    .then(function () {
+                        alert("Register success");
+                    }).catch(function (error) {
+                        alert("Error:" + error);
+                    })
+            })
+        }
+        catch { }
+        console.log('Not register web page');
+    },
+    login() {
+        login.addEventListener("click", () => {
+            let username = document.querySelector("#username").value;
+            let password = document.querySelector("#password").value;
+            const starCountRef = ref(database, 'users/' + username);
+            onValue(starCountRef, (snapshot) => {
+                const data = snapshot.val();
+                if(password === data.password){
+                    alert('帳號密碼正確，正在登陸...');
+                }
+            });
         })
     }
 }
 
 data.create();
+data.login();
